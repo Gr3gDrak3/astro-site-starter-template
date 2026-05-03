ad88953d7be625f200b2e8212193cbfcdad9c530
Gr3gDrak3 <139575690+Gr3gDrak3@users.noreply.github.com>
2026-05-03 18:02:05 -0500

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "About Greg Drake";
+export const SITE_DESCRIPTION = "A quick way to get to know me and my cybersec work.";
